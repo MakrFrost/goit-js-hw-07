a0193d50af7a588c12437b1be4d4c2abf9224f34
MakrFrost <frostmark@ukr.net>
2022-08-19 21:13:12 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryAlbum = document.querySelector(".gallery");
+galleryAlbum.addEventListener("click", openGalleryItem);
+
+//? Открывает модальное окно с картинкой по клику (большой, оригинальной)
+function openGalleryItem(event) {
+  console.log(event.target);
+}
+
+//? Добавляет картинки из объекта в разметку
+function addItemToGallery() {
+  for (const item of galleryItems) {
+    galleryAlbum.insertAdjacentHTML(
+      "afterbegin",
+      `<img src="${item.preview}" alt="${item.description}" >`
+    );
+  }
+}
+addItemToGallery();
