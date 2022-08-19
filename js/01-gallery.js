4ff2115441b45d59063ac967426897b2afbcf8a3
MakrFrost <frostmark@ukr.net>
2022-08-19 21:53:50 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,6 @@ import { galleryItems } from "./gallery-items.js";
 // console.log(galleryItems);
 
 const galleryAlbum = document.querySelector(".gallery");
-galleryAlbum.addEventListener("click", openGalleryItem);
 
 //? Добавляет картинки из объекта в разметку
 function addItemToGallery() {
@@ -23,16 +22,22 @@ function addItemToGallery() {
 }
 addItemToGallery();
 
+galleryAlbum.addEventListener("click", openGalleryItem);
+
 //? Открывает модальное окно с картинкой по клику (большой, оригинальной)
 function openGalleryItem(event) {
   const imageItem = document.querySelector(".gallery__image");
   event.preventDefault();
+
+  for (const data of galleryItems) {
+    console.log(data.dataset.source);
+  }
+
+  //   console.log(imageItem.dataset.source);
   //   console.log(event.target);
-  //   console.log(document.querySelector("[data-source]"));
-  console.log(imageItem.dataset.source);
 }
 
-//? Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+//! Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 //? Реалізація делегування на div.gallery і отримання url великого зображення.
 //? Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
 //? Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
