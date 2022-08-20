923c81fa51546c58738b5eee9a5fbc1f3c58767e
MakrFrost <frostmark@ukr.net>
2022-08-20 13:38:03 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ import { galleryItems } from "./gallery-items.js";
 const galleryAlbum = document.querySelector(".gallery");
 galleryAlbum.addEventListener("click", openGalleryItem);
 
-//? Добавляет картинки из объекта в разметку
+// Добавляет картинки из объекта в разметку
 function addItemToGallery({ preview, original, description }) {
   return `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
@@ -17,22 +17,27 @@ function addItemToGallery({ preview, original, description }) {
     </div>`;
 }
 
+// Делаем один бойльшой файл разметки
 let addNewImage = galleryItems.map(addItemToGallery).join("");
 // console.log(addNewImage);
 
+// Вставляет в разметку разметку картинки
 galleryAlbum.insertAdjacentHTML("afterbegin", addNewImage);
 // console.log(galleryAlbum);
 
-//? Открывает модальное окно с картинкой по клику (большой, оригинальной)
+// Открывает модальное окно с картинкой по клику (большой, оригинальной)
 function openGalleryItem(event) {
+  // Запрещает все события
   event.preventDefault();
+  // Достукиваемся до дата-атрибута по клику!
   const imageSrc = event.target.dataset.source;
-  console.log(imageSrc);
+  // console.log(imageSrc);
   // console.log(event.target);
+  basicLightbox
+    .create(
+      `
+		<img src="${imageSrc}">
+	`
+    )
+    .show();
 }
-
-//! Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-//! Реалізація делегування на div.gallery і отримання url великого зображення.
-//? Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-//? Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-//? Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
